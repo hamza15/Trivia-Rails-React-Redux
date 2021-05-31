e500415fe7a7887fbd3dd4a10a04e7d38fa76505
hamza15 <sardan5@vt.edu>
2021-05-31 18:21:22 -0400

    README

--- frontend/src/redux/actions/selectedQuestions.js
+++ frontend/src/redux/actions/selectedQuestions.js
@@ -2,15 +2,3 @@ export const selectedQuestions = (selectedQuestions) => {
 
     return { type: "SELECTED_QUESTIONS_SUCCESS", payload: selectedQuestions};
 }
-
-// export const selectedQuestions = (category) => {
-//     return dispatch => {
-//         fetch(`http://localhost:3001/categories/${category}`)
-//         .then(res => res.json())
-//         .then(data => 
-//             dispatch({ type: "SELECTED_CATEGORY_SUCCESS", payload: data.map(c => c)}));
-//             // console.log(data.data))
-//     }
-//     // debugger
-//     // return { type: "" }
-// }
